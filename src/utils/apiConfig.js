d603488ea9450e842f5f68cf20df818d5b10fac8
Thisal Deelaka <165908864+ThisalDeelaka@users.noreply.github.com>
2025-04-29 01:11:47 +0530

    added backend docker url

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,5 +1,5 @@
 // src/utils/apiConfig.js
-const API_BASE_URL = "http://localhost:5000/api/auth"; 
+const API_BASE_URL = "http://localhosuser-service:5000/api/auth"; 
 const apiConfig = {
   login: `${API_BASE_URL}/login`,
 };
