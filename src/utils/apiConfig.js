2c66641430a3865889d41f45ae35da3a6c91e1dc
Matheesha Mihiranga <133772302+MatheeshaMihiranga@users.noreply.github.com>
2025-04-29 22:05:15 +0530

    Fix Register User

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -2,6 +2,7 @@
 const API_BASE_URL = "http://localhost:5000/api/auth"; 
 const apiConfig = {
   login: `${API_BASE_URL}/login`,
+  register: `${API_BASE_URL}/register`,
 };
 
 export default apiConfig;
